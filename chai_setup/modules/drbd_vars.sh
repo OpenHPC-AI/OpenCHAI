554bf6fd74d5383409bad44159fe6bbfeb51306a
Satish Gupta <gsatish.june@gmail.com>
2025-12-12 12:41:05 +0530

    Create drbd_vars.sh

---
+++ chai_setup/modules/drbd_vars.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+ALL_YML="../../automation/ansible/group_vars/all.yml"
+
+echo ">>> Configuring DRBD / HA Variables"
+
+fields=(
+  private_interface
+  xcat_vip_ip
+  xcat_vip_interface
+  xcat_vip_subnet_prefix
+  xcat_vip_resource_name
+  drbd_resource_name
+  drbd_clone_name
+  drbd_fs_resource_name
+  drbd_device
+  drbd_mount_dir
+  drbd_fstype
+  pcs_primary_master_node_hostname
+  pcs_secondary_master_node_hostname
+  pcs_cluster_password
+  pcs_cluster_name
+)
+
+for key in "${fields[@]}"; do
+    cur=$(grep "^$key:" "$ALL_YML" | awk '{print $2}')
+    read -p "$key [$cur]: " new
+    new="${new:-$cur}"
+    sed -i "s|^$key:.*|$key: $new|" "$ALL_YML"
+done
+
+echo "✔ DRBD/HA Variables Updated."
