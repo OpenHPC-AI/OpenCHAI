e19b2b0b4feaaaf912f14ccfcfd76b4e42415df2
Satish Gupta <gsatish.june@gmail.com>
2026-01-09 22:46:00 +0530

    Update pxe_server_setup.sh

--- servicenodes/pxe_server_setup.sh
+++ servicenodes/pxe_server_setup.sh
@@ -5,7 +5,7 @@ base_dir="/OpenCHAI"
 
 pxe_server_script="$base_dir/automation/python/run_pxe_server.py"
 pxe_server_playbook="$base_dir/automation/ansible/playbook_library/provision/pxe_server.yml"
-dhcp_file="$PWD/dhcp_clients_mac.txt"
+dhcp_file="$base_dir/servicenodes/dhcp_clients_mac.txt"
 
 # Optional limit (host or group)
 LIMIT_NODE="${1:-}"
